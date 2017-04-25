5a1e36e56e905ccd9388bc2d0bb6444e11dadabf
Leo <leonoragilmour@sky.com>
2017-04-25 13:08:48 +0100

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# WP Sponsored Hub Plugin
+A plugin to create Sponsored Hubs
---
+++ wp-sponsored-hub-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: WP Sponsored Hub Plugin
+* Plugin URI: http://github.com/shortlist-digital/wp-sponsored-hub-plugin
+* Description: A plugin to create Sponsored Hubs
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class WpSponsoredHubPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new WpSponsoredHubPlugin();
